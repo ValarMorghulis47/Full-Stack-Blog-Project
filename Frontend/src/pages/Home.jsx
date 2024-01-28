788e097d27b7fe55d3f00a1cde4145b8ad70dadd
ValarMorghulis47 <myasir47991@gmail.com>
2024-01-28 21:22:50 +0500

    fixed the again api call in home and my post

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,38 +1,41 @@
 import React, { useEffect } from 'react'
 import '../App.css'
 import { Container, PostCard } from '../components'
-import { useSelector , useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Loading from '../components/Loading';
 import { useState } from 'react';
 import { AllPost } from '../store/postSlice';
 function Home() {
     console.log("Home component rendering");
     const [loading, setLoading] = useState(false);
-    const AllPosts = useSelector((state)=> state.post.AllPost)
-    const IsLoggedIn = useSelector((state)=> state.auth.IsLoggedIn)
+    const AllPosts = useSelector((state) => state.post.AllPost)
+    const IsLoggedIn = useSelector((state) => state.auth.IsLoggedIn)
     const dispatch = useDispatch()
     useEffect(() => {
         const fetchPosts = async () => {
-            try {
-                console.log("use effect of home triggered");
-                const response = await fetch(`${import.meta.env.VITE_BASE_URI}/api/v1/posts/`, {
-                    method: 'GET',
-                    credentials: 'include'
-                });
-                if (response.ok) {
-                    console.log("i am in if");
-                    const postsData = await response.json();
-                    dispatch(AllPost(postsData.data))
+            if (!AllPosts?.length) {
+                try {
+                    console.log("use effect of home triggered");
+                    const response = await fetch(`${import.meta.env.VITE_BASE_URI}/api/v1/posts/`, {
+                        method: 'GET',
+                        credentials: 'include'
+                    });
+                    if (response.ok) {
+                        console.log("i am in if");
+                        const postsData = await response.json();
+                        dispatch(AllPost(postsData.data))
+                        setLoading(false);
+                    }
+                    else {
+                        setLoading(false);
+                    }
+                } catch (error) {
+                    // Handle errors here
                     setLoading(false);
+                    // console.error("Error fetching posts:", error);
                 }
-                else {
-                    setLoading(false);
-                }
-            } catch (error) {
-                // Handle errors here
-                setLoading(false);
-                // console.error("Error fetching posts:", error);
             }
+            setLoading(false);
         };
         setLoading(true);
         fetchPosts();
@@ -41,7 +44,7 @@ function Home() {
         return (
             <div className="w-full py-8 text-center height" >
                 <Container>
-                {loading && <Loading />}
+                    {loading && <Loading />}
                     <div className="flex flex-wrap main-container">
                         <div className="p-7 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
@@ -54,11 +57,11 @@ function Home() {
         )
     }
 
-    else if(AllPosts?.length==0){
+    else if (AllPosts?.length == 0) {
         return (
             <div className="w-full py-8 mt-4 text-center height">
                 <Container>
-                {loading && <Loading />}
+                    {loading && <Loading />}
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
@@ -74,7 +77,7 @@ function Home() {
         return (
             <div className='w-full py-8 height'>
                 <Container>
-                {loading && <Loading />}
+                    {loading && <Loading />}
                     <div className='flex flex-wrap'>
                         {AllPosts?.map((post) => (
                             <div key={post._id} className='p-2 w-1/4'>
--- Frontend/src/pages/MyPosts.jsx
+++ Frontend/src/pages/MyPosts.jsx
@@ -8,71 +8,74 @@ import "../App.css"
 function AllPosts() {
     console.log("my post page mounted");
     const UserPosts = useSelector((state) => state.post.UserPost)
-    const userData = useSelector((state)=> state.auth.userData)
+    const userData = useSelector((state) => state.auth.userData)
     const dispatch = useDispatch()
     const [loading, setLoading] = useState(false);
     useEffect(() => {
         const fetchPosts = async () => {
-            try {
-                console.log("use effect of my post triggered");
-                setLoading(true);
-                const response = await fetch(`${import.meta.env.VITE_BASE_URI}/api/v1/posts/user/${userData._id}`,{
-                    method: 'GET',
-                    credentials: 'include',
-                    cache: 'no-cache'
-                })
-                // console.log(await response.json());
-                if (response.ok) {
-                    const postsData = await response.json();
-                    // console.log(postsData.data);
-                    dispatch(UserPost(postsData.data))
+            if (!UserPosts?.length) {
+                try {
+                    console.log("use effect of my post triggered");
+                    setLoading(true);
+                    const response = await fetch(`${import.meta.env.VITE_BASE_URI}/api/v1/posts/user/${userData._id}`, {
+                        method: 'GET',
+                        credentials: 'include',
+                        cache: 'no-cache'
+                    })
+                    // console.log(await response.json());
+                    if (response.ok) {
+                        const postsData = await response.json();
+                        // console.log(postsData.data);
+                        dispatch(UserPost(postsData.data))
+                        setLoading(false);
+                    }
+                    else {
+                        const error = await response.json();
+                        console.log(error);
+                        setLoading(false);
+                    }
+                } catch (error) {
+                    // Handle errors here
                     setLoading(false);
+                    console.error("Error fetching posts:", error);
                 }
-                else {
-                    const error = await response.json();
-                    console.log(error);
-                    setLoading(false);
-                }
-            } catch (error) {
-                // Handle errors here
-                setLoading(false);
-                console.error("Error fetching posts:", error);
             }
+            setLoading(false)
         };
         fetchPosts();
     }, [])
-        if (UserPosts?.length==0) {
-            return (
-                <div className="w-full py-8 mt-4 text-center height">
-                    <Container>
+    if (UserPosts?.length == 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center height">
+                <Container>
                     {loading && <Loading />}
-                        <div className="flex flex-wrap">
-                            <div className="p-2 w-full">
-                                <h1 className="text-2xl font-bold hover:text-gray-500">
-                                    Add a post to see one
-                                </h1>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Add a post to see one
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='w-full py-8 height'>
+                <Container>
+                    {loading && <Loading />}
+                    <div className='flex flex-wrap'>
+                        {UserPosts?.map((post) => (
+                            <div key={post._id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
                             </div>
-                        </div>
-                    </Container>
-                </div>
-            )
-        }
-        else {
-            return (
-                <div className='w-full py-8 height'>
-                    <Container>
-                    {loading && <Loading />}
-                        <div className='flex flex-wrap'>
-                            {UserPosts?.map((post) => (
-                                <div key={post._id} className='p-2 w-1/4'>
-                                    <PostCard {...post} />
-                                </div>
-                            ))}
-                        </div>
-                    </Container>
-                </div>
-            )
-        }
+                        ))}
+                    </div>
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default AllPosts
